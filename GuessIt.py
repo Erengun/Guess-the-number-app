0a8ea556e0503dd4e116de3348be887b68e0e52b
Eren Gun <70534390+Erengun@users.noreply.github.com>
2020-12-07 15:25:44 +0300

    Added Languge
    I added English Languge support now the app supports: Turkish and English.

--- GuessIt.py
+++ GuessIt.py
@@ -1,41 +1,64 @@
 """
 1-100 arasında rastgele üretilecek bir sayıyı aşağı yukarı ifadeleri ile 
 buldurmaya çalışın.
-* Hak bilgisini kullanıcıdan alın ve her soru belirtilen can sayısı
+** "random modülü" için "python random" şeklinde arama yapın.
+*** 100 üzerinden puanlama yapın. Her soru 20 puan.
+**** Hak bilgisini kullanıcıdan alın ve her soru belirtilen can sayısı
      ürzerinden hesaplansın.
 """
 
-"""
-A number between 1-100 will be randomly generated and app will help you with more or less expressions.
-try to find out.
-* Get the right information from the user and the number of lives specified in each question
-      Let it be calculated on it.
-"""
-
 import random
-
 can = 1
 sayi = 0
 tahmin = 0
 deneme=0
-sayi = random.randint(0,100)
-can = int(input("Kaç can istersin :) : "))
-while 0>can or can>20:
-      can = int(input("Kaç can istersin (0-20 arasında) : "))
-else:
-      print("--------- \nPeki, hadi başlayalım")
-      while can > 0:
-            print(f"Kalan can: {can}")
-            tahmin = int(input("Tahmini Alayım : "))
-            deneme += 1
-            if tahmin == sayi:
-                  print(f"Tebrikler! {deneme}. denemede bildin")
-                  break
-            else:
-                  can -= 1
-            if sayi - tahmin > 0:
-                  print(" \nYukarı\n ")
-            elif sayi - tahmin < 0:
-                  print(" \nAşağı\n ")
+
+Lang = int(input("Choose Languge: \nPress 1 for English. \nPress 2 for Turkish\n:"))
+while 0> Lang or Lang>2:
+      Lang = int(input("You Entered a wrong value please try again :"))
+if Lang==2:
+      
+      sayi = random.randint(0,100)
+      can = int(input("Kaç can istersin :) : "))
+      while 0>can or can>20:
+            can = int(input("Kaç can istersin (0-20 arasında) : "))
       else:
-            print(f"Can hakkın bitti kank \nSayı {sayi}'di")
+            print("--------- \nPeki, hadi başlayalım")
+            while can > 0:
+                  print(f"Kalan can: {can}")
+                  tahmin = int(input("Tahmini Alayım : "))
+                  deneme += 1
+                  if tahmin == sayi:
+                        print(f"Tebrikler! {deneme}. denemede bildin")
+                        break
+                  else:
+                        can -= 1
+                  if sayi - tahmin > 0:
+                        print(" \nYukarı\n ")
+                  elif sayi - tahmin < 0:
+                        print(" \nAşağı\n ")
+            else:
+                  print(f"Can hakkın bitti kank \nSayı {sayi}'di")
+elif Lang ==1:
+      sayi = random.randint(0,100)
+      can = int(input("How many lives do you want? : "))
+      while 0>can or can>20:
+            can = int(input("It must be between 0-20 : "))
+      else:
+            print("--------- \nOkay, Let's Start")
+            while can > 0:
+                  print(f"Hearths Left: {can}")
+                  tahmin = int(input("Guess the Number : "))
+                  deneme += 1
+                  if tahmin == sayi:
+                        print(f"Congratulations! You find the number at {deneme}. try. ")
+                        break
+                  else:
+                        can -= 1
+                  if sayi - tahmin > 0:
+                        print(" \nUp\n ")
+                  elif sayi - tahmin < 0:
+                         print(" \nDown\n ")
+            else:
+                  print(f"Your hearts are over. \nThe number was {sayi}")
+      
